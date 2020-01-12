2627c120f347355c137679d462189332a4fb0636
Silvestre <SANTIAGOSILVESTRE@kpmg.com.br>
2020-01-12 14:29:34 -0300

    Trabalhando em tela de serviços

--- app/src/main/java/com/babeirosemgloria/barbergloria/activity/Servico.java
+++ app/src/main/java/com/babeirosemgloria/barbergloria/activity/Servico.java
@@ -5,20 +5,41 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 import com.babeirosemgloria.barbergloria.R;
+
+import java.util.ArrayList;
 
 public class Servico extends AppCompatActivity {
 
     private Button btnCancelar;
+    private ListView listView;
+    private ArrayAdapter adapter;
+    private ArrayList<String>  servicos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_servico);
 
+
+        servicos = new ArrayList<>();
+        servicos.add("Teste");
+        servicos.add("Teste 2");
+        servicos.add("TEste 3");
         btnCancelar = findViewById(R.id.btnCancelar);
+        // listView = findViewById(R.id.lvServicos);
+
+        adapter = new ArrayAdapter(
+            getApplication(),
+            android.R.layout.simple_list_item_1,
+            servicos
+        );
+
+        //listView.setAdapter( adapter );
 
         btnCancelar.setOnClickListener(new View.OnClickListener() {
             @Override
