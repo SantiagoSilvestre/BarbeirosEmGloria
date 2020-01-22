16c608aded9d5b072e1ca24c1fd5fd6287df9f96
Silvestre <SANTIAGOSILVESTRE@kpmg.com.br>
2020-01-22 00:08:41 -0300

    modificações na tela de serviços

--- app/src/main/java/com/babeirosemgloria/barbergloria/activity/Servico.java
+++ app/src/main/java/com/babeirosemgloria/barbergloria/activity/Servico.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.babeirosemgloria.barbergloria.R;
 
@@ -68,12 +70,78 @@ public class Servico extends AppCompatActivity {
         hidratacao = findViewById(R.id.checkHidratacao);
         corRela = findViewById(R.id.checkCorRelax);
         txtValor = findViewById(R.id.txtValor);
-        teste = findViewById(R.id.checkBarba);
-        String bar = barba.getText().toString();
-        txtValor.setText(bar);
 
-        if (!teste.isChecked()){
-            txtValor.setText("00,00");
+        // Aqui recebe as strings do activity
+        String valBarba = barba.getText().toString();
+        String valCorte = corte.getText().toString();
+        String valCorBar = corBarb.getText().toString();
+        String valCorProg = corProg.getText().toString();
+        String valCorRel = corRela.getText().toString();
+        String valHidratacao = hidratacao.getText().toString();
+
+        // Aqui substitui as vírgulas pelo ponto
+        String vBarba = valBarba.replace(",", ".");
+        String vCorte = valCorte.replace(",", ".");
+        String vCorteBar = valCorBar.replace(",", ".");
+        String vCorteProg = valCorProg.replace(",", ".");
+        String vCorterel = valCorRel.replace(",", ".");
+        String vHidratacoa = valHidratacao.replace(",", ".");
+
+
+        // aqui pega os valores e transforma em double
+        Double valorBarba = Double.parseDouble(vBarba);
+        double valorCorte = Double.parseDouble(vCorte);
+        double valorCorBarba = Double.parseDouble(vCorteBar);
+        double valorCorProg = Double.parseDouble(vCorteProg);
+        double valorCorRel = Double.parseDouble(vCorterel);
+        double valorHidratacao = Double.parseDouble(vHidratacoa);
+
+        double valor = 0;
+
+
+        if (barba.isChecked()) {
+            valor += valorBarba;
+            String teste = String.valueOf(valor);
+            Log.i("testeBar", teste);
+            //txtValor.setText((int) valor);
+        } else {
+            valor -= valorBarba;
+            //txtValor.setText((int) valor);
         }
+        if (corte.isChecked()) {
+            valor += valorCorte;
+            txtValor.setText((int) valor);
+        } else {
+            valor -= valorCorte;
+            txtValor.setText((int) valor);
+        }
+        if (corBarb.isChecked()) {
+            valor += valorCorBarba;
+        } else {
+            valor -= valorCorBarba;
+            txtValor.setText((int) valor);
+        }
+        if (corProg.isChecked()) {
+            valor += valorCorProg;
+            txtValor.setText((int) valor);
+        } else {
+            valor -= valorCorProg;
+            txtValor.setText((int) valor);
+        }
+        if (corRela.isChecked()){
+            valor += valorCorRel;
+            txtValor.setText((int) valor);
+        } else {
+            valor -= valorCorRel;
+            txtValor.setText((int) valor);
+        }
+        if (hidratacao.isChecked()) {
+            valor += valorHidratacao;
+            txtValor.setText((int) valor);
+        } else {
+            valor -= valorHidratacao;
+            txtValor.setText((int) valor);
+        }
+
     }
 }
