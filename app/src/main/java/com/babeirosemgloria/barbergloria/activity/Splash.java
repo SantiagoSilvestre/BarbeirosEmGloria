d8bf5b1d6db28b045a05eb04e9e7fc0caef57d61
santiagosilvestre <santiagosilvestre@kpmg.com.br>
2020-03-24 20:12:30 -0300

    lógica em splash

--- app/src/main/java/com/babeirosemgloria/barbergloria/activity/Splash.java
+++ app/src/main/java/com/babeirosemgloria/barbergloria/activity/Splash.java
@@ -4,9 +4,14 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Base64;
+import android.util.Log;
 
 import com.babeirosemgloria.barbergloria.R;
 import com.babeirosemgloria.barbergloria.config.ConfiguracaoFirebase;
@@ -16,6 +21,9 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Splash extends AppCompatActivity {
 
@@ -29,6 +37,22 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
+            try {
+                PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+                for (Signature signature : info.signatures) {
+                    MessageDigest md = MessageDigest.getInstance("SHA");
+                    md.update(signature.toByteArray());
+                    String hashKey = new String(Base64.encode(md.digest(), 0));
+                    Log.i("dsjvcbfhj", "printHashKey() Hash Key: " + hashKey);
+                }
+            } catch (NoSuchAlgorithmException e) {
+                Log.e("dsjvcbfhj", "printHashKey()", e);
+            } catch (Exception e) {
+                Log.e("dsjvcbfhj", "printHashKey()", e);
+            }
+
 
         autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
 
